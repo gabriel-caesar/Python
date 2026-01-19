59ce74b44c638281ccaa109bb7284c86d0cbc6fc
gabriel-caesar <gabriel.mdonno@hotmail.com>
2026-01-19 14:49:44 -0500

    adding warrior travails

--- pygame/tower-defense/utils.py
+++
@@ -1,7 +0,0 @@
-from os import path
-import pygame
-
-def load_image(img_name, subpath):
-  dir = path.dirname(__file__) # gets the file's directory absolute path
-  img_path = path.join(dir, *subpath, img_name) # creates the image path
-  return pygame.image.load(img_path) # returns the image surface
